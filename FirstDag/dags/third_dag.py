9baf885fbd8d69aff9807ae8926bc212912c5707
Nardelli17 <vitornardelli17@gmail.com>
2024-12-06 18:07:05 -0300

    Add script with bash and python operator dag

--- FirstDag/dags/third_dag.py
+++ FirstDag/dags/third_dag.py
@@ -1,7 +1,7 @@
 from airflow import DAG
 from datetime import datetime
 from airflow.operators.python import PythonOperator
-from airflow.operators.bash import BashOperator
+from airflow.operators.bash import BashOperator 
 
 with DAG(dag_id='third_dag', schedule='@daily', 
         start_date=datetime(2024, 1, 1), catchup=False,
